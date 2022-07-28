8a128db616b0555b22d7e675e5051f342beefd93
cone011 <32248132+cone011@users.noreply.github.com>
2022-07-28 07:06:37 -0400

    commit
    commit

---
+++ src/components/UI/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,12 @@
+import classes from "./LoadingSpinner.module.css";
+
+const LoadingSpinner = (props) => {
+  return (
+    <div className={classes.spinner}>
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+};
