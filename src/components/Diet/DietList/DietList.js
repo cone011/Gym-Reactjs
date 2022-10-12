41875db7945c06cea302de4895450f771336f529
cone011 <32248132+cone011@users.noreply.github.com>
2022-10-12 16:56:11 -0300

    commit and push
    commit and push

--- src/components/Diet/DietList/DietList.js
+++ src/components/Diet/DietList/DietList.js
@@ -1,3 +1,4 @@
+import { Fragment, useReducer } from "react";
 import classes from "./DietList.module.css";
 import Card from "../../UI/Card/Card";
 import DataGrid, {
@@ -11,6 +12,25 @@ import DataGrid, {
 import { useHistory } from "react-router-dom";
 import DietDetailPage from "../../../pages/Diet/DietDetailPage";
 import LoadingForm from "../../UI/LoadingForm/LoadingForm";
+import ErrorMessage from "../../UI/ErrorMessage/ErrorMessage";
+
+const deleteReducer = (curDelete, action) => {
+  switch (action.type) {
+    case "BEGIN":
+      return {
+        isShowing: true,
+        message: action.message,
+        IdEliminar: action.IdEliminar,
+      };
+    case "CLOSED":
+      return { ...curDelete, isShowing: false };
+    case "END":
+      return { ...curDelete, isShowing: false };
+
+    default:
+      throw new Error("No se pudo realizar la accion");
+  }
+};
 
 const loadingReducer = (curLoading, action) => {
   switch (action) {
@@ -29,6 +49,16 @@ const loadingReducer = (curLoading, action) => {
 
 const DietList = (props) => {
   const history = useHistory();
+  const [httpLoading, dispatchLoading] = useReducer(loadingReducer, {
+    isLoading: false,
+    error: false,
+    message: null,
+  });
+  const [httpDelete, dispatchDelete] = useReducer(deleteReducer, {
+    isShowing: true,
+    message: null,
+    IdEliminar: null,
+  });
 
   const editDietButtonHandler = (eventValue) => {
     history.push({
@@ -71,41 +101,49 @@ const DietList = (props) => {
   };
 
   return (
-    <div>
-      <Card className={classes.tableCenteredDiet}>
-        {props.isEditable && (
-          <div className={classes.newDiet} onClick={newButtonHandler}>
-            <button>Nueva Dieta</button>
-          </div>
-        )}
-        <DataGrid
-          dataSource={props.dietList}
-          allowColumnReordering={true}
-          rowAlternationEnabled={true}
-          showBorders={true}
-        >
-          <Selection mode="single" />
-          <FilterRow visible={true} applyFilter={true} />
-          <HeaderFilter visible={true} />
-          <MasterDetail enabled={true} component={showDietDetail} />
-          <Column dataField="IdDieta" caption="#" dataType="number" />
-          <Column dataField="Alumno" caption="Alumno" dataType="string" />
-          <Column dataField="Trainner" caption="Trainner" dataType="string" />
-          <Column dataField="FechaCarga" caption="Fecha" dataType="date" />
+    <Fragment>
+      <div>
+        <Card className={classes.tableCenteredDiet}>
           {props.isEditable && (
-            <Column type="buttons">
-              <Button
-                name="editar"
-                cssClass="btn"
-                onClick={editDietButtonHandler}
-              >
-                Editar
-              </Button>
-            </Column>
+            <div className={classes.newDiet} onClick={newButtonHandler}>
+              <button>Nueva Dieta</button>
+            </div>
           )}
-        </DataGrid>
-      </Card>
-    </div>
+          <DataGrid
+            dataSource={props.dietList}
+            allowColumnReordering={true}
+            rowAlternationEnabled={true}
+            showBorders={true}
+          >
+            <Selection mode="single" />
+            <FilterRow visible={true} applyFilter={true} />
+            <HeaderFilter visible={true} />
+            <MasterDetail enabled={true} component={showDietDetail} />
+            <Column dataField="IdDieta" caption="#" dataType="number" />
+            <Column dataField="Alumno" caption="Alumno" dataType="string" />
+            <Column dataField="Trainner" caption="Trainner" dataType="string" />
+            <Column dataField="FechaCarga" caption="Fecha" dataType="date" />
+            {props.isEditable && (
+              <Column type="buttons">
+                <Button
+                  name="editar"
+                  cssClass="btn"
+                  onClick={editDietButtonHandler}
+                >
+                  Editar
+                </Button>
+              </Column>
+            )}
+          </DataGrid>
+        </Card>
+      </div>
+      {httpLoading.isLoading && (
+        <LoadingForm
+          showModal={httpLoading.isLoading}
+          message={httpLoading.message}
+        />
+      )}
+    </Fragment>
   );
 };
 
